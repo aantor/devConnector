411523bae9ceff5ae1d9383dc1ea7b6eab3af2e8
antor <antorinfo@gmail.com>
2021-11-25 10:36:47 +0600

    request & body validation

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express()
 // Connect Database
 connectDB()
 
-// Middleware
-app.use(express.json())
+// Init Middleware
+app.use(express.json({ extended: false }))
 
 app.get("/", (req, res) => {
   res.send("Server working 🔥")
