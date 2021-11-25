f1011d0e120183ab80b119fdc01d776b0f424f29
antor <antorinfo@gmail.com>
2021-11-25 10:04:18 +0600

    route files with express router

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const connectDB = require('./config/db');
+const express = require("express")
+const mongoose = require("mongoose")
+const connectDB = require("./config/db")
 
-const app = express();
-
-// Middleware
-app.use(express.json());
+const app = express()
 
 // Connect Database
 connectDB()
 
+// Middleware
+app.use(express.json())
+
 app.get("/", (req, res) => {
-  res.send("Server working 🔥");
-});
+  res.send("Server working 🔥")
+})
 
+// Define Routes
+app.use("/api/users", require("./routes/api/users"))
+app.use("/api/auth", require("./routes/api/auth"))
+app.use("/api/profile", require("./routes/api/profile"))
+app.use("/api/posts", require("./routes/api/posts"))
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => `Server running on port port 🔥`);
-
-
-
-
+app.listen(port, () => `Server running on port port 🔥`)
