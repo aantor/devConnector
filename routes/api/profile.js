b8cc7660bd13c1dda4c67745aea6d7bb7b449e3d
antor <antorinfo@gmail.com>
2021-11-25 21:27:59 +0600

    create profile model & get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,29 @@
 const express = require("express")
+const { StatusCodes } = require("http-status-codes")
 const router = express.Router()
+const auth = require("../../middleware/auth")
+const Profile = require("../../models/Profile")
 
-// @route     GET api/profile
-// @desc      Profile route
-// @access    Public
-router.get("/", (req, res) => {
-  res.send("profile route")
+// @route     GET api/profile/me
+// @desc      Get current users profile
+// @access    Private
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["name", "avatar"]
+    ) // user is ObjectId from ProfileSchema -- populate user model
+
+    if (!profile) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "There is no profile for this user" })
+    }
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Server error")
+  }
 })
 
 module.exports = router
